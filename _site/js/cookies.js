f89e3f4c4bc07fa49df8f87a9e1bbe5391d6a44b
swoolf <junkwolfspam@gmail.com>
2018-10-29 21:20:59 -0400

    pw works

---
+++ _site/js/cookies.js
@@ -0,0 +1,34 @@
+var status = ""
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+          c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+      }
+  }
+  return "";
+}
+
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function checkLogin(){
+  status = getCookie("status");
+  if (status == "wedding") {
+  } else if(status == "secret"){
+    // window.location.replace("topsecret.html");
+  } else {
+     window.location.replace("password.html");
+     }
+}
